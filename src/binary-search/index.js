d3ab217da122ee892f60cd06facd6820acddda75
shivark <shiva.razzaghkarimi@gmail.com>
2019-02-05 14:05:31 +0000

    Completed binary search and added unit tests

--- src/binary-search/index.js
+++ src/binary-search/index.js
@@ -1,31 +1,35 @@
 const search = (list, t) => {
-    var index = -1;
     var arr = list.sort((a, b) => a - b);
+    console.log(arr);
     var l = arr.length - 1;
-
-    while (index < 0) {
-        find(arr, t, 0, l);
-    }
-
-    return index;
-
-    function find(arr, t, left, right) {
-        var mid = Math.floor((right - left) / 2);
-
-        if (arr[mid] == t) {
-            index = mid
-        }
-
-        if (arr[mid] > t) {
-            find(0, mid - 1)
-        }
-
-        if (arr[mid] < t) {
-            find(mid + 1, l - 1)
-        }
-    }
+    return find(arr, t, 0, l);
 }
 
+const find = (arr, t, left, right) => {
+    var mid = Math.floor((right + left) / 2);
 
+    if (right - left <= 1) {
+        if (arr[right] == t) {
+            return right;
+        }
+        if (arr[left] == t) {
+            return left;
+        } else {
+            return -1;
+        }
+    }
+
+    if (arr[mid] == t) {
+        return mid;
+    }
+
+    if (arr[mid] > t) {
+        return find(arr, t, 0, mid - 1);
+    }
+
+    if (arr[mid] < t) {
+        return find(arr, t, mid + 1, right);
+    }
+}
 
 export default search;
--- src/binary-search/index.spec.js
+++ src/binary-search/index.spec.js
@@ -1,7 +1,32 @@
 import target from './index';
-// [2,5,26,576,123,673,324]
+
 fdescribe('binary search', () => {
-    it('should return middle item', () => {
-        expect(target([3, 4, 7, 9, 24, 64, 35, 34, 67, 45], 24)).toBe(4);
+    const oddArray = [3, 4, 7, 9, 24, 64, 35, 34, 67, 45, 70]; //11
+    const evenArray = [3, 4, 7, 9, 24, 64, 35, 34, 67, 45]; // 10
+
+    it('should return item index in even length array', () => {
+        expect(target(evenArray, 24)).toBe(4);
+    });
+
+    it('should return item index in odd length array', () => {
+        expect(target(oddArray, 24)).toBe(4);
+    });
+
+    it('should return -1 when item does not exists in even length array', () => {
+        expect(target(evenArray, 36)).toBe(-1);
     })
+
+    it('should return -1 when item does not exists in odd length array', () => {
+        expect(target(oddArray, 71)).toBe(-1);
+    })
+
+    it('should return item in end start of the array', () => {
+        expect(target(oddArray, 3)).toBe(0);
+    });
+
+    it('should return item in the end of the array', () => {
+        expect(target(oddArray, 70)).toBe(10);
+    });
 });
+
+
